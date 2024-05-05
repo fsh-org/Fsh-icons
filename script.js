85b61ebb8a4d5097652f13b9478d19e0d11ac91f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-05 23:57:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let icons = ['square'];
 
-document.getElementById('icons').innerHTML = icons.map(i=>`<img src="/icons/base/${i}.svg">`).join('')
+document.getElementById('icons').innerHTML = icons.map(i=>`<img class="icon" src="/icons/base/${i}.svg">`).join('')
 
 document.getElementById('type').onchange = function(event){
-  event.target.toggled
+  document.querySelectorAll('.icon').forEach(e=>{
+    if (event.target.checked) {
+      e.href = e.href.replace('base/', 'outline/')
+    } else {
+      e.href = e.href.replace('outline/', 'base/')
+    }
+  })
 }
