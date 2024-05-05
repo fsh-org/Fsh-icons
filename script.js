220de7dc5aa223ca6aaa9578104d16c27bd1c419
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-05 23:51:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let icons = ['square'];
 
-document.getElementById('icons').innerHTML = icons.map(i=>`<img src="/icons/${i}.svg">`).join('')
+document.getElementById('icons').innerHTML = icons.map(i=>`<img src="/icons/base/${i}.svg">`).join('')
+
+document.getElementById('type').onchange = function(event){
+  event.target.toggled
+}
