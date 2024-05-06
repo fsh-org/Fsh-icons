7d517e40c8fe07ccafaa33494a00c6403c028151
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-06 19:27:43 +0000

    Mag

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let icons = ['backslash', 'bars', 'check', 'circle', 'divide', 'hashtag', 'minus', 'plus', 'slash', 'square', 'star', 'user', 'x'];
+let icons = ['backslash', 'bars', 'check', 'circle', 'divide', 'hashtag', 'magnifying-glass', 'minus', 'plus', 'slash', 'square', 'star', 'user', 'x'];
 
 function render() {
   document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<div class="icon">
