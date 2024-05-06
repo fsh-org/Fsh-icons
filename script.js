c89c31ebdefdb4b7203b7e3ee4a340138e30614b
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-06 20:27:27 +0000

    Changer

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let icons = ['backslash', 'bars', 'check', 'circle', 'divide', 'hashtag', 'magnifying-glass', 'minus', 'plus', 'slash', 'square', 'star', 'user', 'x'];
+let icons = ['backslash', 'bars', 'check', 'circle', 'divide', 'hashtag', 'home', 'magnifying-glass', 'magnifying-glass-right', 'minus', 'plus', 'slash', 'square', 'star', 'user', 'x'];
 
 function render() {
   document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<div class="icon">
-  <p>${i}</p>
   <img src="/icons/${document.getElementById('type').checked ? 'outline' : 'base'}/${i}.svg">
+  <p>${i}</p>
 </div>`).join('')
 }
 render();
