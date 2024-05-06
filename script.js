fd2410686af00ff439e58315495ccfd60a4baf43
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-06 16:40:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let icons = ['square', 'plus'];
+let icons = ['square', 'plus', 'minus', 'divide', 'slash', 'backslash'];
 
 function render() {
-  document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<img class="icon" src="/icons/${document.getElementById('type').checked}/${i}.svg">`).join('')
+  document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<div class="icon">
+  <p>${i}</p>
+  <img class="icon" src="/icons/${document.getElementById('type').checked}/${i}.svg">
+</div>`).join('')
 }
 
 document.getElementById('type').onchange = render;
