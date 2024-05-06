a7985b8deca5e5e767023098a4f3977c8e0e648c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-06 16:50:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let icons = ['square', 'plus', 'minus', 'divide', 'slash', 'backslash'];
 function render() {
   document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<div class="icon">
   <p>${i}</p>
-  <img class="icon" src="/icons/${document.getElementById('type').checked ? 'outline' : 'base'}/${i}.svg">
+  <img src="/icons/${document.getElementById('type').checked ? 'outline' : 'base'}/${i}.svg">
 </div>`).join('')
 }
 
