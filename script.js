a89f496869ae54c69a8bb18a8e66ef6a8a44afa0
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-29 14:08:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let icons = ['backslash', 'bars', 'book', 'check', 'circle', 'divide', 'hashtag', 'home', 'house', 'magnifying-glass', 'magnifying-glass-right', 'minus', 'plus', 'slash', 'square', 'star', 'user', 'x','bold','carret-down','carret-left','carret-right','carret-up','clear-format','clock','ellipsis-vertical','ellipsis','fullscreen-exit','fullscreen','grid-connected','grid','history','indentation-decrease','indentation-increase','italic','layers','list-checkboxes','list','maximize','minimize','order-letters','redo','reload','spellcheck','strikethrough','text-align-center','text-align-justified','text-align-left','text-align-right','text-interline','text','underline','undo','waffle-menu','zoom-in','zoom-out'];
-
-function render() {
-  document.getElementById('icons').innerHTML = icons.filter(i=>i.includes(document.getElementById('search').value)).sort().map(i=>`<div class="icon">
-  <img src="/icons/${document.getElementById('type').checked ? 'outline' : 'base'}/${i}.svg">
-  <p>${i}</p>
-</div>`).join('')
-}
-render();
